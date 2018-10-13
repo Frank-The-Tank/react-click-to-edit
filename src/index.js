e2e07ea036aba3978b48fc75f7dff7f9ccaaaecd
Frank <frank.garcia@alumnimail.pepperdine.edu>
2018-10-13 11:42:55 -0700

    added fieldAffected propType for endEditing
    for use in Atlas Finance

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ class ClickToEdit extends React.Component {
       onEditMode: false
     });
     if (this.props.endEditing) {
-      this.props.endEditing(this.input.current.value);
+      this.props.endEditing(this.input.current.value, this.input.current.fieldAffected);
     }
   };
   handleEnterKey = e => {
@@ -31,7 +31,7 @@ class ClickToEdit extends React.Component {
         onEditMode: false
       });
       if (this.props.endEditing) {
-        this.props.endEditing(this.input.current.value);
+        this.props.endEditing(this.input.current.value, this.input.current.fieldAffected);
       }
     }
   };
@@ -65,6 +65,7 @@ ClickToEdit.propTypes = {
   wrapperClass: PropTypes.string,
   inputClass: PropTypes.string,
   textClass: PropTypes.string,
+  fieldAffected: PropTypes.string,
   value: PropTypes.string.isRequired,
   endEditing: PropTypes.func.isRequired
 };
