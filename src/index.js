c672705bef50483d7fbe297096ec5600ffc7ae26
Frank <frank.garcia@alumnimail.pepperdine.edu>
2018-10-13 12:01:41 -0700

    test

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ class ClickToEdit extends React.Component {
       onEditMode: false
     });
     if (this.props.endEditing) {
-      this.props.endEditing(this.input.current.value, this.input.current.textClass);
+      this.props.endEditing(this.input.current.value, this.input.current.fieldAffected);
     }
   };
   handleEnterKey = e => {
@@ -45,7 +45,6 @@ class ClickToEdit extends React.Component {
           <input
             type="text"
             autoFocus
-            defaultValue={this.props.value}
             className={classNames("CTE--input", this.props.inputClass)}
             onKeyPress={this.handleEnterKey}
             onBlur={this.getOffEditMode}
